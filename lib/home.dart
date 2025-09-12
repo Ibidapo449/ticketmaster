10c820759d73168eab9cd853b23fbf5fce2d383c
ibidapo449 <ibidapodavid10@gmail.com>
2025-09-12 01:22:12 +0100

    final fix for home branch

--- lib/home.dart
+++ lib/home.dart
@@ -230,7 +230,7 @@ class _HomePageState extends State<HomePage>
             Container(
                 width: MediaQuery.of(context).size.height,
                 decoration: const BoxDecoration(
-                  color: Color(0xff004ee9),
+                  color: Color.fromARGB(255, 11, 11, 11),
                 ),
                 child: Column(
                   children: [
